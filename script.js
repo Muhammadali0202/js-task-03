61334bfb5fc1ee6b728d2b364d7d216524d85add
Muhammadali0202 <muhammadalisajjadakhunzada@gmail.com>
2024-02-18 13:30:34 +0500

    done with js-task-3

---
+++ script.js
@@ -0,0 +1,38 @@
+//WELCOME MESSAGE
+
+// let username = prompt("Enter Your Name: ");
+
+// function welcomeMessage(username) {
+//   alert("We Welcome You  " + username);
+// }
+
+// welcomeMessage(username);
+
+//RANDOM QUOTE GENERATOR
+
+// let quoteArr = [
+//   "The computer was born to solve problems that did not exist before.",
+//   "The only way to do great work is to love what you do.",
+//   "Learning to write programs stretches your mind, and helps you think better, creates a way of thinking about things that I think is helpful in all domains.",
+//   "It's not that we need new ideas, but we need to stop having old ideas.",
+//   "Programming isn't about what you know; it's about what you can figure out.",
+//   "I think it's fair to say that personal computers have become the most empowering tool we've ever created. They're tools of communication, they're tools of creativity, and they can be shaped by their user.",
+//   "The best way to predict the future is to invent it.",
+//   "The computer was born to solve problems that did not exist before.",
+//   "The advance of technology is based on making it fit in so that you don't really even notice it, so it's part of everyday life.",
+//   "The computer was born to solve problems that did not exist before.",
+// ];
+// function generalQuote() {
+//   let randomIndex = Math.floor(Math.random() * quoteArr.length);
+//   return quoteArr[randomIndex];
+// }
+// // let quoteArr = generalQuote();
+// console.log(generalQuote());
+
+//GENERATE RANDOM NUMBER B/W 1 AND 100
+// function generateRandomNumber() {
+//   return Math.floor(Math.random() * 100) + 1;
+// }
+
+// let randomNumber = generateRandomNumber();
+// console.log("Generated Random Number:", randomNumber);
